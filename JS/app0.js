c07c4ae74b6913edca526d2f845cec66469ea000
Prakash Kumar <pk612004@gmail.com>
2026-09-22 11:58:48 +0530

    new learning things like add prompt

---
+++ JS/app0.js
@@ -0,0 +1,12 @@
+console.log("hello javascript")
+
+alert("something is wrong ")
+
+prompt("please enter you name: ")
+
+console.error("this is wrong")
+
+console.warn("this is warning ")
+let sum=10;
+
+console.log(`the sum is ${sum} . ` )
