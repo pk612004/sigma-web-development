d4c79dd15f61e69ca49aee4a1229f7d145601101
Prakash Kumar <pk612004@gmail.com>
2026-09-24 10:28:04 +0530

    new tech

--- JS/app0.js
+++ JS/app0.js
@@ -1,12 +1,20 @@
-console.log("hello javascript")
+// console.log("hello javascript")
 
-alert("something is wrong ")
+// alert("something is wrong ")
 
-prompt("please enter you name: ")
+// prompt("please enter you name: ")
 
-console.error("this is wrong")
+// console.error("this is wrong")
 
-console.warn("this is warning ")
-let sum=10;
+// console.warn("this is warning ")
+// let sum=10;
 
-console.log(`the sum is ${sum} . ` )
+// console.log(`the sum is ${sum} . ` )
+
+
+// let password=prompt("enter your password :");
+// let newPassword=password.trim();
+// console.log(newPassword);
+
+
+let arr=["aman","rahul","bittu"];
